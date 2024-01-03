601007bea731f716e8e13ec673764a731d1a550e
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-03 22:38:43 +0200

    Task3[Add]: Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then(([uploadPhoto, createUser]) => {
+      console.log(`${uploadPhoto.body} ${createUser.firstName} ${createUser.lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
