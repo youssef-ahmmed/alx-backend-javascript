c284b6add05f28086279c7b03c9c843cc7d4ae3c
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-03 22:40:40 +0200

    Task3[Edit]: Handle multiple successful promises {eslint}

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-
   return Promise
     .all([uploadPhoto(), createUser()])
     .then(([uploadPhoto, createUser]) => {
