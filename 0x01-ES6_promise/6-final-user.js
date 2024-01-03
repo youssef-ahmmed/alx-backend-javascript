c4fcd9b36a8714f8ef67dfa6665fe66a8df3faa5
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-03 23:15:25 +0200

    Task6[Add]: Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => {
+      result.map((res) => ({
+        status: res.status,
+        value: res.status === 'fulfilled' ? res.value : String(res.reason),
+      }));
+    });
+}
