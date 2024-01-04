9a62fae15393905864d00068f996ec13c27b92c5
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-04 16:07:08 +0200

    Task11[Add]: EVCar extending

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,16 @@
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
+
+export default EVCar;
