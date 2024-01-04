2f5540b8061f17fe1b665eb81daf6b26996e0208
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-04 14:17:31 +0200

    Task5[Add]: Building class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage === 'undefined') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+}
