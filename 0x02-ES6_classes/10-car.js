ab9592cc030450a9f5f5562863b594d9025de000
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-04 15:55:03 +0200

    Task10[Add]: Vroom

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
+
+export default Car;
