4b1b81f0decafaa6cef164cac9993fbf4ebbd66a
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-08 16:07:59 +0200

    Task9[Add]: Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+const groceriesList = () => {
+  const groceryMap = new Map();
+
+  groceryMap.set('Apples', 10);
+  groceryMap.set('Tomatoes', 10);
+  groceryMap.set('Pasta', 1);
+  groceryMap.set('Rice', 1);
+  groceryMap.set('Banana', 5);
+
+  return groceryMap;
+};
+
+export default groceriesList;
