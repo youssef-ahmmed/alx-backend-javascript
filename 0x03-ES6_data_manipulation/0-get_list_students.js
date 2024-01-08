69ade2ddd2fbe98685725979293ca0ce8914de88
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-08 08:54:56 +0200

    Task0[Edit]: Basic list of objects {apply arrow function}

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,4 @@
-function getListStudents() {
+const getListStudents = () => {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
