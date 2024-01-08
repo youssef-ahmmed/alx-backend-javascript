89aea2a8f249be74418914fffd8284d250ad0b35
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-08 08:50:15 +0200

    Task1[Add]: Mapping to student ids

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+function getListStudentIds(studentList) {
+  if (!Array.isArray(studentList)) {
+    return [];
+  }
+
+  return studentList.map((student) => (student.id));
+}
+
+export default getListStudentIds;
