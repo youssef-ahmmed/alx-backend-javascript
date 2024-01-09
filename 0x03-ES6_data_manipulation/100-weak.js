0eb20da82a04bef644ffc2c092b75485b9fa4c89
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-09 09:31:35 +0200

    Task11[Edit]: Weak link data structure with endpoints {new error}

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 export const queryAPI = (endpoint) => {
   const endpointNumbers = weakMap.get(endpoint) || 0;
   if (endpointNumbers >= 5) {
-    throw Error('Endpoint load is high');
+    throw new Error('Endpoint load is high');
   }
 
   weakMap.set(endpoint, endpointNumbers + 1);
