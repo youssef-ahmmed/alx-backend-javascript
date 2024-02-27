fd9aec67e3f0af4401294cdb64a5fb88786d87e3
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-27 17:49:15 +0200

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+process.stdin.setEncoding('utf-8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data.trim()}`);
+
+  console.log('This important software is now closing');
+  process.exit();
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+  process.exit();
+});
