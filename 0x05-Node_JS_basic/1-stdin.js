bc750b3ffe4c11011c0f01ab6f7a5ddf8325047b
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-27 17:54:15 +0200

    Task1[Edit]: remove exit process

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,9 @@
-process.stdin.setEncoding('utf-8');
-
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.trim()}`);
-
-  console.log('This important software is now closing');
-  process.exit();
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
-  process.exit();
+  process.stdout.write('This important software is now closing');
 });
