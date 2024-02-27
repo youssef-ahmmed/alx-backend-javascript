f9770f9cae8c240264afd9819782df9d2d1e4383
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-27 21:29:47 +0200

    Task2[Edit]: refactor the solution into multiple functions

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,31 +2,43 @@ const fs = require('fs');
 
 const countStudents = (path) => {
   try {
-    const csvData = fs.readFileSync(path, 'utf8');
-    const rows = csvData.trim().split('\n');
-    const results = rows.slice(1).map((row) => row.split(','));
-
-    const fieldsWithFirstName = {};
-
-    results.forEach((entry) => {
-      const field = entry[3];
-      const firstName = entry[0];
-
-      if (!fieldsWithFirstName[field]) {
-        fieldsWithFirstName[field] = [];
-      }
-
-      fieldsWithFirstName[field].push(firstName);
-    });
+    const results = getDataFromCsvFile(path);
+    const fieldsWithFirstName = assignNamesWithFields(results);
 
     console.log(`Number of students: ${results.length}`);
-
-    for (const [key, value] of Object.entries(fieldsWithFirstName)) {
-      const list = value.join(', ');
-      console.log(`Number of students in ${key}: ${value.length}. List: ${list}`);
-    }
+    printFiledStats(fieldsWithFirstName);
   } catch (err) {
     throw new Error('Cannot load the database');
+  }
+};
+
+const getDataFromCsvFile = (path) => {
+  const csvData = fs.readFileSync(path, 'utf8');
+  const rows = csvData.trim().split('\n');
+  return rows.slice(1).map((row) => row.split(','));
+};
+
+const assignNamesWithFields = (results) => {
+  const fieldsWithFirstName = {};
+
+  results.forEach((entry) => {
+    const field = entry[3];
+    const firstName = entry[0];
+
+    if (!fieldsWithFirstName[field]) {
+      fieldsWithFirstName[field] = [];
+    }
+
+    fieldsWithFirstName[field].push(firstName);
+  });
+
+  return fieldsWithFirstName;
+};
+
+const printFiledStats = (fieldsWithName) => {
+  for (const [key, value] of Object.entries(fieldsWithName)) {
+    const list = value.join(', ');
+    console.log(`Number of students in ${key}: ${value.length}. List: ${list}`);
   }
 };
 
