5c5aa9ad0c84999134eb2c162c372a14ca6d4de8
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-28 06:06:06 +0200

    Task3[Edit]: separate read file into multiple functions

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,36 +1,48 @@
 const fs = require('fs');
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-      const rows = data.trim().split('\n');
-      const results = rows.slice(1).map((row) => row.split(','));
+const getListDataFromCsvFile = (csvData) => {
+  const rows = csvData.trim().split('\n');
+  return rows.slice(1).map((row) => row.split(','));
+};
 
-      const fieldsWithFirstName = {};
+const assignNamesWithFields = (results) => {
+  const fieldsWithFirstName = {};
 
-      results.forEach((entry) => {
-        const field = entry[3];
-        const firstName = entry[0];
+  results.forEach((entry) => {
+    const field = entry[3];
+    const firstName = entry[0];
 
-        if (!fieldsWithFirstName[field]) {
-          fieldsWithFirstName[field] = [];
-        }
+    if (!fieldsWithFirstName[field]) {
+      fieldsWithFirstName[field] = [];
+    }
 
-        fieldsWithFirstName[field].push(firstName);
-      });
-
-      console.log(`Number of students: ${results.length}`);
-      for (const [key, value] of Object.entries(fieldsWithFirstName)) {
-        const list = value.join(', ');
-        console.log(`Number of students in ${key}: ${value.length}. List: ${list}`);
-      }
-      resolve();
-    });
+    fieldsWithFirstName[field].push(firstName);
   });
-}
+
+  return fieldsWithFirstName;
+};
+
+const printFieldStats = (fieldsWithName) => {
+  for (const [key, value] of Object.entries(fieldsWithName)) {
+    const list = value.join(', ');
+    console.log(`Number of students in ${key}: ${value.length}. List: ${list}`);
+  }
+};
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const results = getListDataFromCsvFile(data);
+    const fieldsWithFirstName = assignNamesWithFields(results);
+
+    console.log(`Number of students: ${results.length}`);
+    printFieldStats(fieldsWithFirstName);
+    resolve();
+  });
+});
 
 module.exports = countStudents;
