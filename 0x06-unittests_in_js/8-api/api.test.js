b5a66f3cb2eb6d2b2280e122623f96cce6b43d8e
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-29 23:04:34 +0200

    Task8[Add]: Basic Integration testing

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,14 @@
+const request = require('request');
+
+const chai = require('chai');
+const expect = chai.expect;
+
+
+describe('getRequest', function () {
+  it('should get index page', function () {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+    });
+  });
+});
