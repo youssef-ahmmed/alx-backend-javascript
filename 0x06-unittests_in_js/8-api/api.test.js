bcb9c990630e82a54ab274c497710752f90ec4cf
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-29 23:09:35 +0200

    Task8[Edit]: add done after testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,10 +5,11 @@ const expect = chai.expect;
 
 
 describe('getRequest', function () {
-  it('should get index page', function () {
+  it('should get index page', function (done) {
     request.get('http://localhost:7865', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
+      done();
     });
   });
 });
