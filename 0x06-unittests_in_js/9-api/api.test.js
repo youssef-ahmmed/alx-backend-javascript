46b988f3d198f7a7ddea45a10e22f3c7cbeff064
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-29 23:47:23 +0200

    Task8[Add]: Add test for index page

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -5,6 +5,14 @@ const expect = chai.expect;
 
 
 describe('getRequest', function () {
+  it('should get index page', function (done) {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
   it('should get cart page with specific id', function (done) {
     request.get('http://localhost:7865/cart/12', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
