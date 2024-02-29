cc16a95b8b946d4c31725727f2e6e599bf01e6cd
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-29 21:27:02 +0200

    Task4[Add]: Testing with Stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,31 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require("./utils");
+
+
+describe('sendPaymentRequestToApi', function () {
+  it('should usage of the Utils', function () {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+
+    spy.restore();
+  });
+
+  it('should verify console log', function () {
+    const spy = sinon.spy(console, 'log');
+    sinon.stub(Utils, 'calculateNumber').returns(10);
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calledWith('The total is: 10')).to.be.true;
+
+    spy.restore();
+  });
+});
